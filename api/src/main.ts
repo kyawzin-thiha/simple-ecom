82d95f09b717babf44a692db6d5972e373fc4049
Kyaw Zin Thiha <github@kyawzinthiha.dev>
2023-11-20 12:56:52 +0000

    remove cookies and helmet

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,9 +2,6 @@ import type { ConfigService as ConfigServiceType } from "@nestjs/config";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import helmet from "helmet";
-import * as cookieParser from "cookie-parser";
-import * as compression from "compression";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,12 +11,9 @@ async function bootstrap() {
   const config = app.get<ConfigServiceType>(ConfigService);
 
   app.enableCors({
-    origin: "*"
+    origin: config.get("CLIENT_URL"),
+    credentials: true
   });
-  console.log(config.get("CLIENT_URL"));
-  app.use(helmet());
-  app.use(cookieParser(config.get("COOKIE_SECRET")));
-  app.use(compression());
 
   const PORT = config.get("PORT") | 3001;
 
